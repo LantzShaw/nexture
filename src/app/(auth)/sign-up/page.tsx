b858513544acb00a36b3288d1f14707545444a39
Lantz <lantzshaw@163.com>
2024-11-16 16:11:07 +0800

    feat: add new features

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,29 @@
+'use client'
+
+import { useState } from 'react'
+
+import './page.css'
+
 export default function SignUpPage() {
+  // NOTE: https://juejin.cn/post/7196843994030342200
+
+  const [isFold, setIsFold] = useState(false)
+
+  const onClick = () => {
+    setIsFold(prev => {
+      return !prev
+    })
+  }
+
   return (
     <>
       <div>Sign Up Page</div>
+      <div>
+        <button onClick={onClick}>{isFold ? 'Unfold' : 'Fold'}</button>
+      </div>
+      <div className={isFold ? 'wrap' : 'wrap active'}>
+        <div style={{ minHeight: 0 }}>some text123</div>
+      </div>
     </>
   )
 }
